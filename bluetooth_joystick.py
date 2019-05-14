65042d3172cd8abe0cf052703cdeee9f3f67aef5
Daniel Sendula <natdan@users.noreply.github.com>
2019-05-14 17:48:38 +0100

    Refactored examples

---
+++ bluetooth_joystick.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+#
+# Copyright 2019 Games Creators Club
+#
+# MIT License
+#
+
+# Based on https://github.com/yaptb/BlogCode
+
+import argparse
+import importlib
+import os
+import sys
+import time
+
+from dbus.mainloop.glib import DBusGMainLoop
+from bt_service import BTDevice
+
+if not os.geteuid() == 0:
+    sys.exit("Only root can run this script")
+
+
+class BluetoothJoystick:
+    def __init__(self, joystick):
+        self.joystick = joystick
+
+    def run(self):
+        DBusGMainLoop(set_as_default=True)
+
+        bt = BTDevice()
+
+        while True:
+            re_start = False
+
+            print("Waiting for connections")
+            bt.listen()
+
+            button_bits_1 = 0
+            button_bits_2 = 0
+
+            axis = [0, 0, 0, 0]
+            new_axis = [0, 0, 0, 0]
+
+            while not re_start:
+                time.sleep(0.1)
+
+                joystick_axis = self.joystick.readAxis()
+                joystick_buttons = self.joystick.readButtons()
+
+                new_axis[0] = joystick_axis['x']
+                new_axis[1] = joystick_axis['y']
+                new_axis[2] = joystick_axis['rx']
+                new_axis[3] = joystick_axis['ry']
+
+                new_button_bits_1 = 0
+                new_button_bits_2 = 0
+
+                # 'trigger', 'tl', 'tr', 'thumb', 'dpad_up', 'dpad_down', 'dpad_left', 'dpad_right', 'thumbl', 'thumbr'
+                if joystick_buttons['dpad_up']:
+                    new_button_bits_1 |= 1
+                if joystick_buttons['dpad_down']:
+                    new_button_bits_1 |= 2
+                if joystick_buttons['dpad_left']:
+                    new_button_bits_1 |= 4
+                if joystick_buttons['dpad_right']:
+                    new_button_bits_1 |= 8
+
+                if joystick_buttons['trigger']:
+                    new_button_bits_1 |= 16
+                if joystick_buttons['tl']:
+                    new_button_bits_1 |= 32
+                if joystick_buttons['tr']:
+                    new_button_bits_1 |= 64
+                if joystick_buttons['thumb']:
+                    new_button_bits_1 |= 128
+
+                if joystick_buttons['thumbl']:
+                    new_button_bits_2 |= 1
+                if joystick_buttons['thumbr']:
+                    new_button_bits_2 |= 2
+
+                has_changes = False
+
+                for i in range(0, 4):
+                    if axis[i] != new_axis[i]:
+                        axis[i] = new_axis[i]
+                        has_changes = True
+
+                if button_bits_1 != new_button_bits_1 or button_bits_2 != new_button_bits_2:
+                    button_bits_1 = new_button_bits_1
+                    button_bits_2 = new_button_bits_2
+                    has_changes = True
+
+                if has_changes:
+                    data = bytes((0xA1, 0x01, button_bits_1, button_bits_2, axis[0], axis[1], axis[2], axis[3]))
+
+                    # print("Changing data " + str(["{:02x}".format(d) for d in data]))
+                    try:
+                        bt.send_message(data)
+                    except Exception as e:
+                        print("Failed to send data - disconnected " + str(e))
+                        re_start = True
---
+++ examples/explorer_phat_joystick.py
@@ -0,0 +1,138 @@
+#
+# Copyright 2019 Games Creators Club
+#
+# MIT License
+#
+
+from smbus import SMBus
+import RPi.GPIO as GPIO
+import time
+
+from joystick import Joystick
+
+
+class ExplorerPHatJoystick(Joystick):
+    # Based on https://github.com/pimoroni/explorer-hat/blob/master/library/explorerhat/ads1015.py
+
+    UP = 18
+    DOWN = 27
+    LEFT = 0
+    RIGHT = 17
+
+    LB = 1
+    RB = 5
+
+    TRIGGER = 25
+    TR = 23
+    TL = 24
+    THUMB = 22
+
+    PGA_6_144V = 6144
+    PGA_4_096V = 4096
+    PGA_2_048V = 2048
+    PGA_1_024V = 1024
+    PGA_0_512V = 512
+    PGA_0_256V = 256
+
+    REG_CONV = 0x00
+    REG_CFG = 0x01
+
+    def __init__(self):
+        self.address = 0x48
+        self.i2c = SMBus(1)
+
+        self.samples_per_second_map = {128: 0x0000, 250: 0x0020, 490: 0x0040, 920: 0x0060, 1600: 0x0080, 2400: 0x00A0, 3300: 0x00C0}
+        self.channel_map = {0: 0x4000, 1: 0x5000, 2: 0x6000, 3: 0x7000}
+        self.programmable_gain_map = {6144: 0x0000, 4096: 0x0200, 2048: 0x0400, 1024: 0x0600, 512: 0x0800, 256: 0x0A00}
+
+        self.axis = { 'x': 0.0, 'y': 0.0, 'rx': 0.0, 'ry': 0.0 }
+        self.buttons = { 'trigger': False, 'tl': False, 'tr': False, 'thumb': False, 'dpad_up': False, 'dpad_down': False, 'dpad_left': False, 'dpad_right': False, 'thumbl': False, 'thumbr': False }
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+
+        GPIO.setup(ExplorerPHatJoystick.UP, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.DOWN, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.LEFT, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.RIGHT, GPIO.IN)
+
+        GPIO.setup(ExplorerPHatJoystick.LB, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.RB, GPIO.IN)
+
+        GPIO.setup(ExplorerPHatJoystick.TRIGGER, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.TR, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.TL, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.THUMB, GPIO.IN)
+
+    def _read_se_adc(self, channel=1, programmable_gain=6144, samples_per_second=1600):
+        # sane defaults
+        config = 0x0003 | 0x0100
+
+        config |= self.samples_per_second_map[samples_per_second]
+        config |= self.channel_map[channel]
+        config |= self.programmable_gain_map[programmable_gain]
+
+        # set "single shot" mode
+        config |= 0x8000
+
+        # write single conversion flag
+        self.i2c.write_i2c_block_data(self.address, ExplorerPHatJoystick.REG_CFG, [(config >> 8) & 0xFF, config & 0xFF])
+
+        delay = (1.0 / samples_per_second) + 0.0001
+        time.sleep(delay)
+
+        data = self.i2c.read_i2c_block_data(self.address, ExplorerPHatJoystick.REG_CONV)
+
+        return (((data[0] << 8) | data[1]) >> 4) * programmable_gain / 2048.0 / 1000.0
+
+    @staticmethod
+    def _fix(v):
+        r = int(((v - 2.5) / 2.5) * 127)
+        if r < 0:
+            r = 256 + r
+        elif r > 127:
+            r = 127
+
+        try:
+            c = chr(r)
+        except Exception as e:
+            print("Failed to convert " + str(v) + " got " + str(r))
+            r = 0
+
+        # print("Read joystick value as " + str(v) + " fixed it to " + str(r))
+        return r
+
+    def readAxis(self):
+        self.axis['rx'] = self._fix(self._read_se_adc(3))
+        self.axis['ry'] = self._fix(self._read_se_adc(0))
+        self.axis['x'] = self._fix(self._read_se_adc(2))
+        self.axis['y'] = self._fix(self._read_se_adc(1))
+        return self.axis
+
+    def readButtons(self):
+        self.buttons['dpad_up'] = not bool(GPIO.input(ExplorerPHatJoystick.UP))
+        self.buttons['dpad_down'] = not bool(GPIO.input(ExplorerPHatJoystick.DOWN))
+        self.buttons['dpad_left'] = not bool(GPIO.input(ExplorerPHatJoystick.LEFT))
+        self.buttons['dpad_right'] = not bool(GPIO.input(ExplorerPHatJoystick.RIGHT))
+
+        self.buttons['thumbl'] = not bool(GPIO.input(ExplorerPHatJoystick.LB))
+        self.buttons['thumbr'] = not bool(GPIO.input(ExplorerPHatJoystick.RB))
+
+        self.buttons['trigger'] = not bool(GPIO.input(ExplorerPHatJoystick.TRIGGER))
+        self.buttons['tl'] = not bool(GPIO.input(ExplorerPHatJoystick.TL))
+        self.buttons['tr'] = not bool(GPIO.input(ExplorerPHatJoystick.TR))
+        self.buttons['thumb'] = not bool(GPIO.input(ExplorerPHatJoystick.THUMB))
+
+        return self.buttons
+
+
+if __name__ == "__main__":
+
+    import sys, os
+    sys.path.append(os.path.split(os.path.dirname(os.path.realpath(__file__)))[0])
+
+    from bluetooth_joystick import BluetoothJoystick
+    from exporer_phat_joystick import ExplorerPHatJoystick
+
+    bluetooth_joystick = BluetoothJoystick(ExplorerPHatJoystick())
+    bluetooth_joystick.run()
