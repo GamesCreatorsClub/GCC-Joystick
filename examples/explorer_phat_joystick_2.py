7d41e35d5e6dd6f9e6578ed395145eb44ce25c95
Daniel Sendula <natdan@users.noreply.github.com>
2019-05-27 12:55:06 +0100

    Joystick interface and BluetoothJoystickDeviceMain implementation with example

---
+++ examples/explorer_phat_joystick_2.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+
+#
+# Copyright 2019 Games Creators Club
+#
+# MIT License
+#
+
+import os
+import sys
+import RPi.GPIO as GPIO
+import time
+
+from smbus import SMBus
+from bt_joystick import Joystick, BluetoothJoystickDeviceMain
+
+
+class ExplorerPHatJoystick(Joystick):
+    # Based on https://github.com/pimoroni/explorer-hat/blob/master/library/explorerhat/ads1015.py
+
+    UP = 18
+    DOWN = 27
+    LEFT = 0
+    RIGHT = 17
+
+    LB = 1
+    RB = 5
+
+    TRIGGER = 25
+    TR = 23
+    TL = 24
+    THUMB = 22
+
+    PGA_6_144V = 6144
+    PGA_4_096V = 4096
+    PGA_2_048V = 2048
+    PGA_1_024V = 1024
+    PGA_0_512V = 512
+    PGA_0_256V = 256
+
+    REG_CONV = 0x00
+    REG_CFG = 0x01
+
+    def __init__(self):
+        self.address = 0x48
+        self.i2c = SMBus(1)
+
+        self.samples_per_second_map = {128: 0x0000, 250: 0x0020, 490: 0x0040, 920: 0x0060, 1600: 0x0080, 2400: 0x00A0, 3300: 0x00C0}
+        self.channel_map = {0: 0x4000, 1: 0x5000, 2: 0x6000, 3: 0x7000}
+        self.programmable_gain_map = {6144: 0x0000, 4096: 0x0200, 2048: 0x0400, 1024: 0x0600, 512: 0x0800, 256: 0x0A00}
+
+        self.axes = {Joystick.X: 0.0, Joystick.Y: 0.0, Joystick.Rx: 0.0, Joystick.Ry: 0.0}
+        self.buttons = {'trigger': False, 'tl': False, 'tr': False, 'thumb': False, 'dpad_up': False, 'dpad_down': False, 'dpad_left': False, 'dpad_right': False, 'thumbl': False, 'thumbr': False}
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+
+        GPIO.setup(ExplorerPHatJoystick.UP, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.DOWN, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.LEFT, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.RIGHT, GPIO.IN)
+
+        GPIO.setup(ExplorerPHatJoystick.LB, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.RB, GPIO.IN)
+
+        GPIO.setup(ExplorerPHatJoystick.TRIGGER, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.TR, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.TL, GPIO.IN)
+        GPIO.setup(ExplorerPHatJoystick.THUMB, GPIO.IN)
+
+    def _read_se_adc(self, channel=1, programmable_gain=6144, samples_per_second=1600):
+        # sane defaults
+        config = 0x0003 | 0x0100
+
+        config |= self.samples_per_second_map[samples_per_second]
+        config |= self.channel_map[channel]
+        config |= self.programmable_gain_map[programmable_gain]
+
+        # set "single shot" mode
+        config |= 0x8000
+
+        # write single conversion flag
+        self.i2c.write_i2c_block_data(self.address, ExplorerPHatJoystick.REG_CFG, [(config >> 8) & 0xFF, config & 0xFF])
+
+        delay = (1.0 / samples_per_second) + 0.0001
+        time.sleep(delay)
+
+        data = self.i2c.read_i2c_block_data(self.address, ExplorerPHatJoystick.REG_CONV)
+
+        return (((data[0] << 8) | data[1]) >> 4) * programmable_gain / 2048.0 / 1000.0
+
+    @staticmethod
+    def _fix(v):
+        r = int(((v - 2.5) / 2.5) * 127)
+        if r < 0:
+            r = 256 + r
+        elif r > 127:
+            r = 127
+
+        try:
+            c = chr(r)
+        except Exception as e:
+            print("Failed to convert " + str(v) + " got " + str(r))
+            r = 0
+
+        # print("Read joystick value as " + str(v) + " fixed it to " + str(r))
+        return r
+
+    @property
+    def defined_button_number(self):
+        return 14
+
+    @property
+    def defined_axes(self):
+        return [Joystick.X, Joystick.Y, Joystick.Rx, Joystick.Ry]
+
+    def read_axes(self):
+        self.axes[Joystick.Rx] = self._fix(self._read_se_adc(3))
+        self.axes[Joystick.Ry] = self._fix(self._read_se_adc(0))
+        self.axes[Joystick.X] = self._fix(self._read_se_adc(2))
+        self.axes[Joystick.Y] = self._fix(self._read_se_adc(1))
+        return self.axes
+
+    def read_buttons(self):
+        self.buttons['dpad_up'] = not bool(GPIO.input(ExplorerPHatJoystick.UP))
+        self.buttons['dpad_down'] = not bool(GPIO.input(ExplorerPHatJoystick.DOWN))
+        self.buttons['dpad_left'] = not bool(GPIO.input(ExplorerPHatJoystick.LEFT))
+        self.buttons['dpad_right'] = not bool(GPIO.input(ExplorerPHatJoystick.RIGHT))
+
+        self.buttons['thumbl'] = not bool(GPIO.input(ExplorerPHatJoystick.LB))
+        self.buttons['thumbr'] = not bool(GPIO.input(ExplorerPHatJoystick.RB))
+
+        self.buttons['trigger'] = not bool(GPIO.input(ExplorerPHatJoystick.TRIGGER))
+        self.buttons['tl'] = not bool(GPIO.input(ExplorerPHatJoystick.TL))
+        self.buttons['tr'] = not bool(GPIO.input(ExplorerPHatJoystick.TR))
+        self.buttons['thumb'] = not bool(GPIO.input(ExplorerPHatJoystick.THUMB))
+
+        return [
+            self.buttons['trigger'],
+            self.buttons['thumbl'],
+            self.buttons['thumbr'],
+            self.buttons['thumb'],
+            self.buttons['tl'],
+            self.buttons['tr'],
+            self.buttons['dpad_up'],
+            self.buttons['dpad_down'],
+            self.buttons['dpad_left'],
+            self.buttons['dpad_right']]
+
+
+if __name__ == "__main__":
+    if not os.geteuid() == 0:
+        sys.exit("Only root can run this script")
+
+    from bt_joystick import BluetoothJoystickDeviceMain
+
+    bluetooth_joystick = BluetoothJoystickDeviceMain(ExplorerPHatJoystick())
+    bluetooth_joystick.run()
--- src/python/bt_joystick/main.py
+++ src/python/bt_joystick/main.py
@@ -12,103 +12,138 @@ import time
 from dbus.mainloop.glib import DBusGMainLoop
 from bt_joystick import BTDevice
 
-# if not os.geteuid() == 0:
-#     sys.exit("Only root can run this script")
+from bt_joystick.hid_report_descriptor import create_joystick_report_descriptor, Usage
 
 
 class Joystick:
 
+    X = Usage.X
+    Y = Usage.Y
+    Z = Usage.Z
+    Rx = Usage.Rx
+    Ry = Usage.Ry
+    Rz = Usage.Rz
+    Slider = Usage.Slider
+    Dial = Usage.Dial
+    Wheel = Usage.Wheel
+
     def __init__(self):
         pass
 
-    def readAxis(self):
-        raise NotImplementedError()
+    @property
+    def defined_kind(self):
+        return Usage.Gamepad
 
-    def readButtons(self):
-        raise NotImplementedError()
+    @property
+    def defined_button_number(self):
+        raise NotImplementedError("Please return number of buttons your joystick implements")
 
-    # TODO Add way for Joystick implementation to pass 'description' to Main class
-    # so it can create appropriate SDP record and handle results
+    @property
+    def defined_axes(self):
+        raise NotImplementedError("Please return defined axes your joystick implements")
+
+    def read_axis(self):
+        raise NotImplementedError("Please return an dictionary with defined axes values")
+
+    def read_buttons(self):
+        raise NotImplementedError("Please return array of 1/0 or 'True'/'False' values for defined buttons")
 
 
 class BluetoothJoystickDeviceMain:
-    def __init__(self, joystick):
+    def __init__(self, joystick, reading_frequency=10):
         self.joystick = joystick
+        self.reading_frequency = reading_frequency
+
+        self.button_number = joystick.defined_button_number
+        self.axes = joystick.defined_axes
+
+        self.only_changes = True
+        self.do_run = True
+
+        self.button_bits = [0] * ((self.button_number - 1) // 8 + 1)
+        self.new_button_bits = [0] * len(self.button_bits)
+
+        self.axes_data = [0] * len(self.axes)
+        self.new_axes_data = [0] * len(self.axes)
+
+        self.hid_descriptor = None
+        self.bt = None
+
+    def init(self):
+        self.hid_descriptor = create_joystick_report_descriptor(
+            kind=self.joystick.defined_kind,
+            axes=self.axes,
+            button_number=self.button_number
+        )
+
+        self.bt = BTDevice(hid_descriptor=self.hid_descriptor)
+
+    def read_joystick_values(self):
+        joystick_axes = self.joystick.read_axes()
+        joystick_buttons = self.joystick.read_buttons()
+
+        i = 0
+        for axis in self.axes:
+            self.new_axes_data[i] = joystick_axes[axis]
+            i += 1
+
+        i = 0
+        bit = 1
+        for button_state in joystick_buttons:
+            if button_state:
+                self.new_button_bits[i] |= bit
+            else:
+                self.new_button_bits[i] &= ~bit
+
+            bit *= 2
+            if bit == 256:
+                bit = 1
+                i += 1
+
+    def has_joystick_changes(self):
+        has_changes = False
+
+        for i in range(0, len(self.axes_data)):
+            if self.axes_data[i] != self.new_axes_data[i]:
+                self.axes_data[i] = self.new_axes_data[i]
+                has_changes = True
+
+        for i in range(0, len(self.new_button_bits)):
+            if self.button_bits[i] != self.new_button_bits[i]:
+                self.button_bits[i] = self.new_button_bits[i]
+                has_changes = True
+
+        return has_changes
+
+    def send_joystick_values(self):
+        data = bytes((0xA1, 0x01)) + bytes(self.button_bits) + bytes(self.new_axes_data)
+
+        # print("Changing data " + str(["{:02x}".format(d) for d in data]))
+        try:
+            self.bt.send_message(data)
+            return True
+        except Exception as e:
+            print("Failed to send data - disconnected " + str(e))
+            return False
+
+    def loop(self):
+        while self.do_run:
+            time.sleep(1 / self.reading_frequency)
+
+            self.read_joystick_values()
+
+            if not self.only_changes or self.has_joystick_changes():
+                successful = self.send_joystick_values()
+                if not successful:
+                    return
 
     def run(self):
         DBusGMainLoop(set_as_default=True)
 
-        bt = BTDevice()
+        self.init()
 
-        while True:
-            re_start = False
-
+        while self.do_run:
             print("Waiting for connections")
-            bt.listen()
+            self.bt.listen()
 
-            button_bits_1 = 0
-            button_bits_2 = 0
-
-            axis = [0, 0, 0, 0]
-            new_axis = [0, 0, 0, 0]
-
-            while not re_start:
-                time.sleep(0.1)
-
-                joystick_axis = self.joystick.readAxis()
-                joystick_buttons = self.joystick.readButtons()
-
-                new_axis[0] = joystick_axis['x']
-                new_axis[1] = joystick_axis['y']
-                new_axis[2] = joystick_axis['rx']
-                new_axis[3] = joystick_axis['ry']
-
-                new_button_bits_1 = 0
-                new_button_bits_2 = 0
-
-                # TODO this should really use Joystick description to determine which buttons are sent as which
-                # 'trigger', 'tl', 'tr', 'thumb', 'dpad_up', 'dpad_down', 'dpad_left', 'dpad_right', 'thumbl', 'thumbr'
-                if joystick_buttons['dpad_up']:
-                    new_button_bits_1 |= 1
-                if joystick_buttons['dpad_down']:
-                    new_button_bits_1 |= 2
-                if joystick_buttons['dpad_left']:
-                    new_button_bits_1 |= 4
-                if joystick_buttons['dpad_right']:
-                    new_button_bits_1 |= 8
-
-                if joystick_buttons['trigger']:
-                    new_button_bits_1 |= 16
-                if joystick_buttons['tl']:
-                    new_button_bits_1 |= 32
-                if joystick_buttons['tr']:
-                    new_button_bits_1 |= 64
-                if joystick_buttons['thumb']:
-                    new_button_bits_1 |= 128
-
-                if joystick_buttons['thumbl']:
-                    new_button_bits_2 |= 1
-                if joystick_buttons['thumbr']:
-                    new_button_bits_2 |= 2
-
-                has_changes = False
-
-                for i in range(0, 4):
-                    if axis[i] != new_axis[i]:
-                        axis[i] = new_axis[i]
-                        has_changes = True
-
-                if button_bits_1 != new_button_bits_1 or button_bits_2 != new_button_bits_2:
-                    button_bits_1 = new_button_bits_1
-                    button_bits_2 = new_button_bits_2
-                    has_changes = True
-
-                if has_changes:
-                    data = bytes((0xA1, 0x01, button_bits_1, button_bits_2, axis[0], axis[1], axis[2], axis[3]))
-
-                    # print("Changing data " + str(["{:02x}".format(d) for d in data]))
-                    try:
-                        bt.send_message(data)
-                    except Exception as e:
-                        print("Failed to send data - disconnected " + str(e))
-                        re_start = True
+            self.loop()
